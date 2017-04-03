09a965e29b519da178edaa8ba47da225c912c2a6
F. Javier R. Donado <fjrd84@gmail.com>
2017-04-04 01:07:45 +0200

    JSON input for the engine. Improved testing.

--- tests/processor_test.py
+++ tests/processor_test.py
@@ -4,21 +4,40 @@ server_test.py
 import analyzer.processor
 from unittest import mock
 
+
 class MockAnalysisUploader(object):
     """
     Mock for the Analysis Uploader class
     """
+
     def __init__(self):
         pass
 
     def upload_analysis(self, analysis_json):
-        pass
+        assert analysis_json['analysis']['health_related'] == "true"
+        assert analysis_json['user_name'] == "jdonado"
+        assert analysis_json['user_description'] == "Some random radiologist."
+        assert analysis_json['query'] == "diabetes"
+
 
 @mock.patch('analyzer.engine')
 def test_process_job(mock_engine):
     """ Testing the base route """
-    example_job = {"message": "Example message"}
+    example_job = {
+        "user_name": "jdonado",
+        "user_description": "Some random radiologist.",
+        "created_at": "2017-04-02T22:35:04.868Z",
+        "message": "Some random message",
+        "source": "twitter",
+        "query": "diabetes"
+    }
     mock_uploader = MockAnalysisUploader()
-    mock_engine.dummy_nlp_analysis.return_value = True
+    mock_engine.dummy_nlp_analysis.return_value = {
+        "health_related": "true",
+        "created_at": "2017-04-02T22:35:04.868Z",
+        "profile": "radiologist",
+        "problem": "diabetes",
+        "solution": "aspirin"
+    }
     analyzer.processor.process_job(example_job, mock_uploader)
-    mock_engine.dummy_nlp_analysis.assert_called_once_with('Example message')
+    mock_engine.dummy_nlp_analysis.assert_called_once_with(example_job)
