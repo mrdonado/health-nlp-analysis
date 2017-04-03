a13769e28631ca77266833ae2e02a353f870e26c
F. Javier R. Donado <fjrd84@gmail.com>
2017-04-04 01:06:51 +0200

    Dummy engine: json input.

--- analyzer/engine.py
+++ analyzer/engine.py
@@ -3,10 +3,23 @@ Example of a script performing an NLP analysis of a given message
 """
 import datetime
 
-def dummy_nlp_analysis(message):
+def dummy_nlp_analysis(input_job):
     """
-    An nlp analysis function returns a JSON with different tags and a
-    classification.
+    An nlp analysis function returns a JSON with the analysis results
+    for a given input JSON (input_job) that contains a message and
+    information about the source.
+
+    Here's an example of the expected JSON format for input_job:
+
+    {
+        "user_name": "jdonado",
+        "user_description": "Some random radiologist.",
+        "created_at": "2017-04-02T22:35:04.868Z",
+        "message": "Some random message",
+        "source": "twitter",
+        "query": "diabetes"
+    }
+
     This function provides just a dummy analizer that can be used as
     a model for future analysis engines.
     """
--- tests/engine_test.py
+++ tests/engine_test.py
@@ -3,8 +3,17 @@ engine_test.py
 """
 from analyzer.engine import dummy_nlp_analysis
 
+
 def test_dummy_nlp_analysis():
-    example_analysis = dummy_nlp_analysis("Example")
+    input_job = {
+        "user_name": "jdonado",
+        "user_description": "Some random radiologist.",
+        "created_at": "2017-04-02T22:35:04.868Z",
+        "message": "Some random message",
+        "source": "twitter",
+        "query": "diabetes"
+    }
+    example_analysis = dummy_nlp_analysis(input_job)
     assert example_analysis["health_related"] == "true"
     assert example_analysis["profile"] == "radiologist"
     assert example_analysis["problem"] == "diabetes"
