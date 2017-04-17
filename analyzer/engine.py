cfe96033151d1f5c3337f16bc5d60a955c5ee2c5
F. Javier R. Donado <fjrd84@gmail.com>
2017-04-17 16:24:32 +0200

    Integrate real nlp analysis.

--- analyzer/engine.py
+++ analyzer/engine.py
@@ -1,7 +1,51 @@
 """
 Example of a script performing an NLP analysis of a given message
 """
-import datetime
+from datetime import datetime
+from analyzer.engines import user_analyzer
+from analyzer.engines import text_analyzer
+## Initialization ##
+
+# User analysis
+DICTIONARY = user_analyzer.dictionary_parser(
+    './language_data/user_dictionary.txt')
+LEXICON = user_analyzer.lexicon_generator(
+    './language_data/user_grammar.txt', DICTIONARY)
+
+
+# Text analysis
+LANGUAGE_DATA = text_analyzer.language_data_loader(
+    './language_data/grammar.txt',
+    './language_data/start_words.txt',
+    './language_data/stop_words.txt'
+)
+
+
+def nlp_analysis(job_json):
+    """
+    It takes a job as an input and returns an analysis.
+    """
+    analysis = dict()
+    # Get 'profile' and 'health_related'
+    analysis['profile'] = user_analyzer.user_analyzer(job_json['user_description'],
+                                                      LEXICON)[1]
+
+    # Identified medical sources will be tagged as health related
+    analysis['health_related'] = analysis['profile'] != '<unknown source>'
+
+    # The text analyzer inferes a health related problem and its solution,
+    # when available
+    text_analysis = text_analyzer.analyzer(job_json['message'],
+                                           LANGUAGE_DATA['start_words'],
+                                           LANGUAGE_DATA['stop_words'],
+                                           LANGUAGE_DATA['grammar'])
+
+    analysis['solution'] = text_analysis[0]
+    analysis['problem'] = text_analysis[1]
+
+    # Save the analysis timestamp
+    analysis['created_at'] = datetime.now().isoformat()
+    return analysis
 
 def dummy_nlp_analysis(input_job):
     """
@@ -25,7 +69,7 @@ def dummy_nlp_analysis(input_job):
     """
     result = {
         "health_related": "true",
-        "created_at": datetime.datetime.now().isoformat(),
+        "created_at": datetime.now().isoformat(),
         "profile": "radiologist",
         "problem": "diabetes",
         "solution": "aspirin"
