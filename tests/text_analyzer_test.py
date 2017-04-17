d811cd654bc384f3d6cfa3beb45c74703aa84b2e
F. Javier R. Donado <fjrd84@gmail.com>
2017-04-17 16:27:03 +0200

    analyzer.engines tests.

---
+++ tests/text_analyzer_test.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Text analyzer tests.
+"""
+from analyzer.engines import text_analyzer
+
+START_WORDS_PATH = './tests/engines/demo_start_words.txt'
+STOP_WORDS_PATH = './tests/engines/demo_stop_words.txt'
+GRAMMAR_PATH = './tests/engines/demo_grammar.txt'
+
+
+def test_file_parser():
+    """
+    File parser tests.
+    """
+    start_words = text_analyzer.file_parser(START_WORDS_PATH, True)
+    assert 'negativity' in start_words
+    assert 'pci' in start_words
+    assert r'acute \w+' in start_words
+    assert 'eczema' in start_words
+    assert 'wrinkles' in start_words
+    assert 'yeast infections' in start_words
+    assert 'wrinkless' not in start_words
+    assert 'invented_word' not in start_words
+
+
+def test_language_data_loader():
+    """
+    Language data loader tests
+    """
+    language_data = text_analyzer.language_data_loader(
+        GRAMMAR_PATH,
+        START_WORDS_PATH,
+        STOP_WORDS_PATH)
+    assert 'eczema' in language_data['start_words']
+    assert '^@\\w+$' in language_data['stop_words']
+    assert '[s] \\w+ed to (healthier|better)( \\S+){0,7} [p]' in language_data['grammar']
+    assert r'[s] effective( \w+){0,2} (in|for|to)( \w+){0,5} [p]' in language_data['grammar']
+
+
+def test_start_word_match():
+    """
+    Start word match tests
+    """
+    result = text_analyzer.start_word_match("Some input message",
+                                            ['one', 'word'])
+    assert result is None
+    result = text_analyzer.start_word_match("Some input message",
+                                            ['input', 'word'])
+    assert result == 'input'
+    result = text_analyzer.start_word_match(
+        "Some input message about anorexia nervosa and other things",
+        ['one', 'word', 'anorexia', 'unrelated', 'anorexia nervosa'])
+    assert result == 'anorexia nervosa'
+
+
+def test_analyzer():
+    """
+    Analyzer tests
+    """
+    language_data = text_analyzer.language_data_loader(
+        GRAMMAR_PATH,
+        START_WORDS_PATH,
+        STOP_WORDS_PATH)
+    message = "#exercise helps regulate mood, can improve depression " \
+        "and helps fight against diabetes, cancer + heart disease. " \
+        "https://t.co/sw6mvsslg"
+    analysis = text_analyzer.analyzer(message,
+                                      language_data['start_words'],
+                                      language_data['stop_words'],
+                                      language_data['grammar'])
+    assert analysis[1] == 'heart disease'
+    assert analysis[0] == 'mood'
+    message = "some unrelated message that only talks about watching tv"
+    analysis = text_analyzer.analyzer(message,
+                                      language_data['start_words'],
+                                      language_data['stop_words'],
+                                      language_data['grammar'])
+    assert analysis[0] == '<nothing_found>'
---
+++ tests/user_analyzer_test.py
@@ -0,0 +1,77 @@
+"""
+Test the user analyzer engine
+"""
+from analyzer.engines import user_analyzer
+
+USER_DICTIONARY = './tests/engines/demo_user_dictionary.txt'
+USER_GRAMMAR = './tests/engines/demo_user_grammar.txt'
+
+
+def test_is_tag():
+    """
+    Test tag identification
+    """
+    # Valid tags
+    assert user_analyzer.is_tag('<THIS_IS_A_TAG>')
+    assert user_analyzer.is_tag('<TAG>')
+    # Invalid tags
+    assert not user_analyzer.is_tag('<THIS_I _A_TAG>')
+    assert not user_analyzer.is_tag('<this is no tag>')
+    assert not user_analyzer.is_tag('<SOME_INVALID_TAG')
+
+
+def test_dictionary_parser():
+    """
+    Parse a demo dictionary
+    """
+    dictionary = user_analyzer.dictionary_parser(USER_DICTIONARY)
+    assert dictionary['<DISEASE>'] == ['liver cancer']
+    assert dictionary['<MEDICAL_FIELD>'] == ['family medicine', 'biotech']
+    assert dictionary['<MEDICAL_ATTRIBUTE>'] == ['family medicine', 'biotech']
+    assert dictionary['<MEDICAL_JOB>'] == ['physicist', 'dermat(ó|o)log(o|a)']
+
+
+def test_lexicon_generator():
+    """
+    Generate a demo lexicon from a demo grammar file
+    """
+
+    dictionary = user_analyzer.dictionary_parser(USER_DICTIONARY)
+    lexicon = user_analyzer.lexicon_generator(USER_GRAMMAR,
+                                              dictionary)
+    assert lexicon == {
+        '¡<MEDICAL_ATTRIBUTE> <MEDICAL_FIELD>¡': [
+            '(family medicine|biotech) (family '
+            'medicine|biotech)',
+            '(family medicine|biotech) (family '
+            'medicine|biotech)'],
+        '¡working for <MEDICAL_ATTRIBUTE>¡': [
+            'working for (family medicine|biotech)',
+            'working for (family medicine|biotech)'],
+        '¡<MEDICAL_ATTRIBUTE> (web|portal|site)¡': [
+            '(family medicine|biotech) '
+            '(web|portal|site)',
+            '(family medicine|biotech) '
+            '(web|portal|site)'],
+        '¡<MEDICAL_JOB>¡ (and|&) \\w+': [
+            '(physicist|dermat(ó|o)log(o|a)) (and|&) '
+            '\\w+',
+            '(physicist|dermat(ó|o)log(o|a))']}
+
+
+def test_user_analyzer():
+    """
+    Test the user analyzer function
+    """
+    dictionary = user_analyzer.dictionary_parser(USER_DICTIONARY)
+    lexicon = user_analyzer.lexicon_generator(USER_GRAMMAR, dictionary)
+
+    assert user_analyzer.user_analyzer('Some random person',
+                                       lexicon) == ['<no pattern>',
+                                                    '<unknown source>']
+    assert user_analyzer.user_analyzer('Some physicist and father',
+                                       lexicon) == ['¡<MEDICAL_JOB>¡ (and|&) \\w+',
+                                                    'physicist']
+    assert user_analyzer.user_analyzer('family medicine website',
+                                       lexicon) == ['¡<MEDICAL_ATTRIBUTE> (web|portal|site)¡',
+                                                    'family medicine web']
