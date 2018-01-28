5c4c5b7f83e8357ce424e8a6d430fe59bff7d80e
Juan Fernández <jnferfer1@gmail.com>
2018-01-29 00:41:09 +0100

    Include few forbidden tokens in get_string_match_plus_noun_phrases()

--- analyzer/engines/magic_bullet_analyzer.py
+++ analyzer/engines/magic_bullet_analyzer.py
@@ -47,8 +47,18 @@ def get_string_match_plus_noun_phrases(magic_bullet_instance, start_word, noun_p
 
     result = [None, noun_phrases]
     
-    forbidden_tokens = ['the', 'new', 'its', 'our']
-
+    forbidden_tokens = [
+        'the',
+        'new',
+        'its',
+        'our',
+        'her',
+        'his',
+        'my',
+        'your',
+        'which'
+    ]
+    
     if magic_bullet_instance in message.lower():
         if len(noun_phrases) == 0:
             for np in NLP(message).noun_chunks:
